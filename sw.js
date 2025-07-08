5e127808d22f9d250e55e94e9f0b52dc093d5438
ABHISHEK ANAND TIWARI <abhishekanandtiwari2006@gmail.com>
2025-07-08 17:26:08 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,39 @@
-self.addEventListener('install', function(e) {
-  console.log('Service Worker Installed');
+const CACHE_NAME = "gamezone-v2";
+const urlsToCache = [
+  "/gamezone.com/",
+  "/gamezone.com/index.html",
+  "/gamezone.com/style.css",
+  "/gamezone.com/script.js",
+  "/gamezone.com/icon-192.png",
+  "/gamezone.com/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request));
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
 });
